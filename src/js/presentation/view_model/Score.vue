7961230f3baf54257e4106c814380a7f96c0e93f
shinpei maruyama <shinpeim@gmail.com>
2017-03-23 19:59:00 +0900

    Create score UI

---
+++ src/js/presentation/view_model/Score.vue
@@ -0,0 +1,54 @@
+<style scoped>
+    .note {
+        width: 80px;
+    }
+</style>
+<template>
+    <div class="row">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title brown-text">Score</span>
+                </div>
+                <div class="card horizontal">
+                    <div class="card-stacked">
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                    </div>
+               </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+            }
+        },
+
+        methods: {
+        }
+    }
+</script>
