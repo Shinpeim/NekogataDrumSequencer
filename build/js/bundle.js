29ee4b0c86643ee458baaa5678c93405758fbdda
shinpei maruyama <shinpeim@gmail.com>
2017-03-23 13:24:32 +0900

    Minimal babel setup

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -68,10 +68,39 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-console.log("hello, world");
+"use strict";
 
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Hello = function () {
+  function Hello() {
+    _classCallCheck(this, Hello);
+
+    this.world = "world";
+  }
+
+  _createClass(Hello, [{
+    key: "say",
+    value: function say() {
+      var _this = this;
+
+      var h = function h(hello) {
+        console.log(hello + " " + _this.world);
+      };
+      h("hello");
+    }
+  }]);
+
+  return Hello;
+}();
+
+var h = new Hello();
+h.say();
 
 /***/ })
 /******/ ]);
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-console.log("hello, world");
+class Hello {
+  constructor() {
+    this.world = "world";
+  }
+
+  say() {
+    const h = (hello) =>  {
+      console.log(hello + " " + this.world);
+    }
+    h("hello");
+  }
+}
+
+const h = new Hello();
+h.say();
