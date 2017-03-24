64748c65d1a1d2a63daffa1f46f8af1e3ffba9fa
shinpei maruyama <shinpeim@gmail.com>
2017-03-25 04:25:05 +0900

    Bootstrap testing environment

---
+++ test/domain/Score.test.js
@@ -0,0 +1,35 @@
+import assert from 'assert';
+import Score from '../../src/js/domain/Score'
+
+describe('Score', () => {
+    let score;
+    before(() => {
+        const notes = [
+            true, false, true, false, true, false, true, false,
+            true, false, true, false, true, false, true, false
+        ];
+
+        score = new Score(notes);
+    });
+
+    describe('constroctor()', () => {
+        it('should have notes passed to constructor', () => {
+            const expected = [
+                true, false, true, false, true, false, true, false,
+                true, false, true, false, true, false, true, false
+            ];
+
+            assert.deepEqual(expected, score.notes);
+        });
+    });
+
+    describe('#toggleNote()', () => {
+        it('should toggle note of index', () => {
+            score.toggleNote(0);
+            assert.equal(false, score.notes[0]);
+
+            score.toggleNote(1);
+            assert.equal(true, score.notes[1]);
+        });
+    });
+});
