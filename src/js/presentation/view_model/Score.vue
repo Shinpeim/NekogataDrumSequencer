91687313473fa73b135a404fce5908202dde18ef
shinpei maruyama <shinpeim@gmail.com>
2017-03-24 10:51:15 +0900

    Create pattern UI

--- src/js/presentation/view_model/Score.vue
+++ src/js/presentation/view_model/Score.vue
@@ -9,11 +9,24 @@
             <div class="card">
                 <div class="card-content">
                     <span class="card-title brown-text">Score</span>
+                    <span class="brown-text">
+                        Track: {{selectedTrack}}
+                    </span>
+                    <div>
+                            <input name="track" type="radio" value="H.H." id="radio-track-1" v-model="selectedTrack"/>
+                            <label for="radio-track-1">H.H.</label>
+                            <input name="track" type="radio" value="S.D" id="radio-track-2" v-model="selectedTrack"/>
+                            <label for="radio-track-2">S.D.</label>
+                            <input name="track" type="radio" value="B.D." id="radio-track-3" v-model="selectedTrack"/>
+                            <label for="radio-track-3">B.D.</label>
+                            <input name="track" type="radio" value="R.S." id="radio-track-4" v-model="selectedTrack"/>
+                            <label for="radio-track-4">R.S.</label>
+                    </div>
                 </div>
                 <div class="card horizontal">
                     <div class="card-stacked">
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                     </div>
@@ -21,19 +34,19 @@
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                     </div>
                     <div class="card-stacked">
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                         <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
-                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note">HH</span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                    </div>
+                    <div class="card-stacked">
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
+                        <div class="card-content cel"><span class="note waves-effect waves-light btn-large"><span class="note"></span></span></div>
                     </div>
                </div>
             </div>
@@ -45,6 +58,7 @@
     export default {
         data(){
             return {
+                selectedTrack: "H.H."
             }
         },
 
