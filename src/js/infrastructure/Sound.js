016bf1b6f5418d29764a3bd676caf22f04de58e7
shinpei maruyama <shinpeim@gmail.com>
2017-03-25 02:50:39 +0900

    Support Mobile Safari

--- src/js/infrastructure/Sound.js
+++ src/js/infrastructure/Sound.js
@@ -1,44 +1,29 @@
+let context = null;
+
 export default class {
     constructor(fileURL){
         this._fileURL = fileURL;
-        this._loaded = false;
         this._buffer = null;
+        try {
+            const AudioContext = window.AudioContext || window.webkitAudioContext;
+            if (context == null) {
+                context = new AudioContext();
+            }
+            console.debug("audio context created");
+        } catch (e) {
+            throw new Error("can't create AudioContext");
+        }
+
     }
 
     setup(){
-        return new Promise((resolve, reject) => {
-            try {
-                const AudioContext = window.AudioContext || window.webkitAudioContext;
-                this._context = new AudioContext();
-                console.debug("audio context created");
-                resolve();
-            } catch (e) {
-                reject(new Error("can't create AudioContext"));
-                return;
-            }
-        }).then(() => {
-            return this._loadBuffer(this._fileURL)
-        }).then(() => {
-            this._loaded = true;
-        })
-    }
-
-    play(){
-        console.debug("playing sound");
-        const source = this._context.createBufferSource();
-        source.buffer = this._buffer;
-        source.connect(this._context.destination);
-        source.start(0);
-    }
-
-    _loadBuffer(){
         const request = new XMLHttpRequest();
         request.open('GET', this._fileURL , true);
         request.responseType = 'arraybuffer';
 
         return new Promise((resolve, reject) => {
             request.onload = () => {
-                this._context.decodeAudioData(request.response, (buffer) => {
+                context.decodeAudioData(request.response, (buffer) => {
                     this._buffer = buffer;
                     resolve();
                 }, (e) => {
@@ -46,6 +31,14 @@ export default class {
                 });
             };
             request.send();
-        })
+        });
+    }
+
+    play(){
+        console.debug("playing sound");
+        const source = context.createBufferSource();
+        source.buffer = this._buffer;
+        source.connect(context.destination);
+        source.start(0);
     }
 }
