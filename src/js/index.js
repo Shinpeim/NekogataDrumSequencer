d9a5df3b7c36c808e4e440361d6bbba37181e649
shinpei maruyama <shinpeim@gmail.com>
2017-09-08 13:20:41 +0900

    jQueryとVue.jsはCDNから読み込みようにした

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-import 'materialize-css/bin/materialize.css';
-import 'materialize-css/bin/materialize.js';
-import 'materialize-css/js/init.js';
-
 import Vue from 'vue'
 import Application from './presentation/vue_components/Application.vue'
 
